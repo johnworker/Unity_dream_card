3f4c1e53979dd9d4326b4f79990d373e6dcc6434
johnworker <besscomdark@gmail.com>
2022-05-31 21:48:21 +0800

    背景

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Draggable : MonoBehaviour
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        Debug.Log ("OnBeginDrag");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        Debug.Log ("OnDrag");
+        this.transform.position = eventData.position;
     }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log ("OnEndDrag");
+    }
+
+
 }
